b08e1341467b1443d9fbdd59b9e464e770aee8c7
COMPAQ_PC <himalhs1@gmail.com>
2017-09-03 23:22:39 +0545

    introduction to socket.id and init function

---
+++ public/js/script.js
@@ -0,0 +1,98 @@
+$(document).ready(function(){
+
+  var socket={};
+  let username;
+  $('#btn').on('click',function(event){
+    socket = io.connect('http://192.168.1.180:3000');
+    event.preventDefault();
+    username = $('#username').val();
+    $('#verify').html('Hi '+username+' now you can chat with your friends');
+    init();
+    socket.emit('init',username);
+  });
+  function init(){
+    var messageBox=$('#message');
+    $('#btn1').click(function(){
+      socket.emit('chat with friend',$(this).html());
+    });
+    // socket.on('message', function (data) {
+    //   console.log(data);
+    //
+    //   if(data!=='denied'){
+    //     $('#verify').html('');
+    //     messageBox.html('<h3>'+data+' is Online!</h3>');
+    //     socket.emit('emit other user', data);
+    //   }
+    //   else{
+    //     messageBox.html('');
+    //     messageBox.html('<h3>Get lost!</h3>');
+    //   }
+    //
+    // });
+    socket.on('new user',function(data){
+      console.log(data);
+      $('#notification').append('<h3>'+data.name+' is Online!</h3>')
+      // messageBox.append('<h3>'+data+' is Online!</h3>');
+    });
+    socket.on('user disconnected',function(data){
+      alert(data);
+    });
+
+
+    //==============Send Message event==================
+    $('#submit-btn').on('click',function(event){
+      event.preventDefault();
+
+      var message = $('#msg-body').val();
+      if(message===''){
+        return false;
+      }
+      else{
+        var sender = username;
+        $('#msg-body').val('').focus();
+        $('html, body').animate({
+            scrollTop: $(document).height()
+        }, 1000);
+        socket.emit('on message',{message,sender});
+        messageBox.append('<div class="me-container"><p class="me"><strong>'+sender+': </strong>'+message+'</p></div>');
+      }
+
+    });
+    //============or on enter===========
+    $('#msg-body').keyup(function(event) {
+      if(event.keyCode!= 8 || event.keyCode!= 46){
+        socket.emit('is typing',username+ ' is typing....');
+      }
+      if (event.keyCode == 13) {
+          $('#submit-btn').trigger('click');
+          return false;
+       }
+
+    });
+
+    socket.on('is typing',function(data){
+      $('#is-typing').html(data);
+      $('#is-typing').show();
+      var st=setTimeout(function(){
+        console.log('helloooooo');
+        $('#is-typing').hide();
+      },3000);
+      clearTimeout(st);
+    });
+
+    socket.on('client',function(data){
+      console.log(data);
+      $('html, body').animate({
+          scrollTop: $(document).height()
+      }, 1000);
+      if(data.sender===username){
+        messageBox.append('<div class="me-container"><p class="me"><strong>'+data.sender+': </strong>'+data.message+'</p></div>');
+      }
+      else{
+        messageBox.append('<div class="other-container"><p class="other"><strong>'+data.sender+': </strong>'+data.message+'</p></div>');
+      }
+    });
+
+
+  }
+});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mysql = require('mysql');
 var path = require('path');
+var fs = require('fs');
 var port = 3000;
 var app = require('express')();
 var server = require('http').Server(app);
@@ -25,26 +26,52 @@ connection.connect(function(err){
 app.get('/',function(req,res){
   res.sendFile('index.html');
 });
+// app.get('/:user',function(req,res){
+//
+// });
 
-
+var online_all=[];
 // connection.end();
 io.on('connection', function (socket) {
+  const sockId=socket.id;
+  socket.on('init',function(msg){
+    socket.username=msg;
+    online_all.push({id:sockId,name:msg});
+    connection.query('SELECT * FROM single_convo WHERE user="'+msg+'" ORDER BY id DESC LIMIT 5 ;', function (error, results, fields) {
+      if (error) throw error;
+      console.log(results);
+      socket.broadcast.emit('new user',{id:sockId,name:msg,convo:results});
+    });
+
+  });
+  socket.on('disconnect',function(){
+    console.log('disonnected: '+sockId);
+    socket.broadcast.emit('user disconnected',socket.username);
+  })
   socket.on('emit other user',function(data){
     console.log('on other user ', data);
     socket.broadcast.emit('emit other user',data);
-  })
-  socket.on('user', function(data){
-    if(data==='HIMAL' || data==='OTHER'){
-      socket.username=data;
-      socket.emit('message',data);
-      console.log(data);
-    }
-    else{
-      socket.emit('message','denied');
-    }
+  });
+  // socket.on('user', function(id,data){
+  //   console.log('envoked');
+  //   if(data==='HIMAL' || data==='OTHER'){
+  //     socket.username=data;
+  //     socket.broadcast.to(id).emit('message',data);
+  //     console.log(data);
+  //   }
+  //   else{
+  //     socket.broadcast.to(id).emit('message','denied');
+  //   }
+  // });
+  socket.on('chat with friend',function(id, msg){
+    console.log(id, msg);
+    socket.broadcast.to(id).emit('emit other user',msg);
   });
   socket.on('on message', function (data) {
     console.log(data);
+    // fs.appendFile('chat.txt',data.message,function(err){
+    //   if(!err) console.log('file writte');
+    // })
     connection.query('INSERT INTO single_convo (convo,user) VALUES ("'+data.message+'","'+data.sender+'")', function (error, results, fields) {
       if (error) throw error;
       console.log(results);
